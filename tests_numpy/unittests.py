592171a325b3139f5c9883bc2f9db590a01aff14
Dr. James R. Maddison <j.r.maddison@ed.ac.uk>
2019-07-02 23:03:54 +0100

    flake8

--- tests_numpy/unittests.py
+++ tests_numpy/unittests.py
@@ -22,16 +22,21 @@ from tlm_adjoint import *
 from tlm_adjoint import manager as _manager
 
 import gc
-import numpy
+import numpy as np
 import unittest
 import weakref
 
 Function_ids = {}
 _orig_Function_init = Function.__init__
+
+
 def _Function__init__(self, *args, **kwargs):
     _orig_Function_init(self, *args, **kwargs)
     Function_ids[self.id()] = weakref.ref(self)
+
+
 Function.__init__ = _Function__init__
+
 
 def leak_check(test):
     def wrapped_test(self, *args, **kwargs):
@@ -41,10 +46,10 @@ def leak_check(test):
 
         # Clear some internal storage that is allowed to keep references
         manager = _manager()
-        manager._cp.clear(clear_refs = True)
-        tlm_values = manager._tlm.values()
+        manager._cp.clear(clear_refs=True)
+        tlm_values = manager._tlm.values()  # noqa: F841
         manager._tlm.clear()
-        tlm_eqs_values = manager._tlm_eqs.values()
+        tlm_eqs_values = manager._tlm_eqs.values()  # noqa: F841
         manager._tlm_eqs.clear()
 
         gc.collect()
@@ -52,7 +57,7 @@ def leak_check(test):
         refs = 0
         for F in Function_ids.values():
             F = F()
-            if not F is None:
+            if F is not None:
                 info("%s referenced" % F.name())
                 refs += 1
         if refs == 0:
@@ -62,31 +67,33 @@ def leak_check(test):
         self.assertEqual(refs, 0)
     return wrapped_test
 
+
 class tests(unittest.TestCase):
     @leak_check
     def test_ContractionSolver(self):
-        reset("memory", {"replace":True})
+        reset("memory", {"replace": True})
         clear_caches()
         stop_manager()
 
         space_0 = FunctionSpace(1)
         space = FunctionSpace(3)
-        A = numpy.array([[1.0, 2.0, 3.0], [0.0, 4.0, 5.0], [0.0, 0.0, 6.0]], dtype = numpy.float64)
+        A = np.array([[1.0, 2.0, 3.0], [0.0, 4.0, 5.0], [0.0, 0.0, 6.0]],
+                     dtype=np.float64)
 
         def forward(m):
-            x = Function(space, name = "x")
+            x = Function(space, name="x")
             ContractionSolver(A, (1,), (m,), x).solve()
 
-            norm_sq = Function(space_0, name = "norm_sq")
+            norm_sq = Function(space_0, name="norm_sq")
             NormSqSolver(x, norm_sq).solve()
 
-            J = Functional(name = "J")
+            J = Functional(name="J")
             NormSqSolver(norm_sq, J.fn()).solve()
 
             return x, J
 
-        m = Function(space, name = "m", static = True)
-        function_set_values(m, numpy.array([7.0, 8.0, 9.0], dtype = numpy.float64))
+        m = Function(space, name="m", static=True)
+        function_set_values(m, np.array([7.0, 8.0, 9.0], dtype=np.float64))
 
         start_manager()
         x, J = forward(m)
@@ -95,60 +102,62 @@ class tests(unittest.TestCase):
         self.assertEqual(abs(A.dot(m.vector()) - x.vector()).max(), 0.0)
 
         dJ = compute_gradient(J, m)
-        min_order = taylor_test(lambda m : forward(m)[1], m, J_val = J.value(), dJ = dJ)
+        min_order = taylor_test(lambda m: forward(m)[1], m, J_val=J.value(),
+                                dJ=dJ)
         self.assertGreater(min_order, 2.00)
 
-        ddJ = Hessian(lambda m : forward(m)[1])
-        min_order = taylor_test(lambda m : forward(m)[1], m, J_val = J.value(), dJ = dJ, ddJ = ddJ)
+        ddJ = Hessian(lambda m: forward(m)[1])
+        min_order = taylor_test(lambda m: forward(m)[1], m, J_val=J.value(),
+                                dJ=dJ, ddJ=ddJ)
         self.assertGreater(min_order, 3.00)
 
     @leak_check
     def test_InnerProductSolver(self):
-        reset("memory", {"replace":True})
+        reset("memory", {"replace": True})
         clear_caches()
         stop_manager()
 
         space = FunctionSpace(10)
 
         def forward(F):
-            G = Function(space, name = "G")
+            G = Function(space, name="G")
             AssignmentSolver(F, G).solve()
 
-            J = Functional(name = "J")
+            J = Functional(name="J")
             InnerProductSolver(F, G, J.fn()).solve()
 
             return J
 
-        F = Function(space, name = "F", static = True)
-        function_set_values(F, numpy.random.random(function_local_size(F)))
+        F = Function(space, name="F", static=True)
+        function_set_values(F, np.random.random(function_local_size(F)))
 
         start_manager()
         J = forward(F)
         stop_manager()
 
         dJ = compute_gradient(J, F)
-        min_order = taylor_test(forward, F, J_val = J.value(), dJ = dJ)
+        min_order = taylor_test(forward, F, J_val=J.value(), dJ=dJ)
         self.assertGreater(min_order, 1.99)
 
     @leak_check
     def test_SumSolver(self):
-        reset("memory", {"replace":True})
+        reset("memory", {"replace": True})
         clear_caches()
         stop_manager()
 
         space = FunctionSpace(10)
 
         def forward(F):
-            G = Function(space, name = "G")
+            G = Function(space, name="G")
             AssignmentSolver(F, G).solve()
 
-            J = Functional(name = "J")
+            J = Functional(name="J")
             SumSolver(G, J.fn()).solve()
 
             return J
 
-        F = Function(space, name = "F", static = True)
-        function_set_values(F, numpy.random.random(function_local_size(F)))
+        F = Function(space, name="F", static=True)
+        function_set_values(F, np.random.random(function_local_size(F)))
 
         start_manager()
         J = forward(F)
@@ -159,10 +168,11 @@ class tests(unittest.TestCase):
         dJ = compute_gradient(J, F)
         self.assertEqual(abs(function_get_values(dJ) - 1.0).max(), 0.0)
 
+
 if __name__ == "__main__":
-    numpy.random.seed(1201)
+    np.random.seed(1201)
     unittest.main()
 
-#  tests().test_ContractionSolver()
-#  tests().test_InnerProductSolver()
-#  tests().test_SumSolver()
+#    tests().test_ContractionSolver()
+#    tests().test_InnerProductSolver()
+#    tests().test_SumSolver()
