ca4ac9c62c104072a8df2a311e4876c004a00ab6
Dr. James R. Maddison <j.r.maddison@ed.ac.uk>
2019-07-22 17:39:05 +0100

    flake8, minor updates

--- python_numpy/tlm_adjoint/equations.py
+++ python_numpy/tlm_adjoint/equations.py
@@ -22,8 +22,7 @@ from .backend_interface import *
 
 from .base_equations import *
 
-import copy
-import numpy
+import numpy as np
 
 __all__ = \
     [
@@ -32,9 +31,10 @@ __all__ = \
         "ContractionSolver"
     ]
 
+
 class ConstantMatrix(Matrix):
-    def __init__(self, A, A_T = None, has_ic_dep = False):
-        Matrix.__init__(self, nl_deps = [], has_ic_dep = has_ic_dep)
+    def __init__(self, A, A_T=None, has_ic_dep=False):
+        Matrix.__init__(self, nl_deps=[], has_ic_dep=has_ic_dep)
         self._A = A
         self._A_T = A_T
 
@@ -44,35 +44,48 @@ class ConstantMatrix(Matrix):
     def A_T(self):
         return self._A.T if self._A_T is None else self._A_T
 
-    def forward_action(self, nl_deps, x, b, method = "assign"):
+    def forward_action(self, nl_deps, x, b, method="assign"):
         sb = self._A.dot(x.vector())
         if method == "assign":
             b.vector()[:] = sb
+        elif method == "add":
+            b.vector()[:] += sb
+        elif method == "sub":
+            b.vector()[:] -= sb
         else:
-            getattr(b.vector()[:], {"add":"__iadd__", "sub":"__isub__"}[method])(sb)
+            raise EquationException(f"Invalid method: '{method:s}'")
 
-    def adjoint_action(self, nl_deps, adj_x, b, b_index = 0, method = "assign"):
-        if b_index != 0: raise EquationException("Invalid index")
+    def adjoint_action(self, nl_deps, adj_x, b, b_index=0, method="assign"):
+        if b_index != 0:
+            raise EquationException("Invalid index")
         sb = self._A_T().dot(x.vector())
         if method == "assign":
             b.vector()[:] = sb
+        elif method == "add":
+            b.vector()[:] += sb
+        elif method == "sub":
+            b.vector()[:] -= sb
         else:
-            getattr(b.vector()[:], {"add":"__iadd__", "sub":"__isub__"}[method])(sb)
+            raise EquationException(f"Invalid method: '{method:s}'")
 
     def forward_solve(self, nl_deps, b):
-        return Function(b.function_space(), _data = numpy.linalg.solve(self._A, b.vector()))
+        return Function(b.function_space(),
+                        _data=np.linalg.solve(self._A, b.vector()))
 
-    def adjoint_derivative_action(self, nl_deps, nl_dep_index, x, adj_x, b, method = "assign"):
-        return
+    def adjoint_derivative_action(self, nl_deps, nl_dep_index, x, adj_x, b,
+                                  method="assign"):
+        raise EquationException("Unexpected call to adjoint_derivative_action")
 
     def adjoint_solve(self, nl_deps, b):
-        return Function(b.function_space(), _data = numpy.linalg.solve(self.A_T(), b.vector()))
+        return Function(b.function_space(),
+                        _data=np.linalg.solve(self.A_T(), b.vector()))
 
     def tangent_linear_rhs(self, M, dM, tlm_map, x):
         return None
 
+
 class ContractionArray:
-    def __init__(self, A, I, A_T = None, alpha = 1.0):
+    def __init__(self, A, I, A_T=None, alpha=1.0):
         for i in range(len(I) - 1):
             if I[i + 1] <= I[i]:
                 raise EquationException("Axes must be in ascending order")
@@ -87,7 +100,7 @@ class ContractionArray:
     def A_T(self):
         return self._A.T if self._A_T is None else self._A_T
 
-    def I(self):
+    def I(self):  # noqa: E743
         return self._I
 
     def alpha(self):
@@ -100,28 +113,32 @@ class ContractionArray:
             v = self._A.dot(X[0].vector())
         else:
             v = self._A
-            for i, (j, x) in enumerate(zip(self._I, X)):
-                v = numpy.tensordot(v, x.vector(), axes = ((j - i,), (0,)))
-        if self._alpha != 1.0:
-            if len(X) > 0:
-                v *= self._alpha
+            if len(self._I) == 0:
+                v = v.copy()
             else:
-                v = v * self._alpha
+                for i, (j, x) in enumerate(zip(self._I, X)):
+                    v = np.tensordot(v, x.vector(), axes=((j - i,), (0,)))
+
+        if self._alpha != 1.0:
+            v *= self._alpha
+
         return v
 
+
 class ContractionRHS(RHS):
-    def __init__(self, A, I, Y, A_T = None, alpha = 1.0):
-        if len(Y) != len(A.shape) - 1:
+    def __init__(self, A, I, X, A_T=None, alpha=1.0):
+        if len(X) != len(A.shape) - 1:
             raise EquationException("Contraction does not result in a vector")
         j = set(range(len(A.shape)))
         for i in I:
-            j.remove(i)  # Raises an error if there is a duplicate element in I
+            # Raises an error if there is a duplicate element in I
+            j.remove(i)
         assert(len(j) == 1)
         j = j.pop()
 
-        RHS.__init__(self, Y, nl_deps = [] if len(Y) == 1 else Y)
+        RHS.__init__(self, X, nl_deps=[] if len(X) == 1 else X)
         self._A_T = A_T
-        self._c = ContractionArray(A, I, A_T = A_T, alpha = alpha)
+        self._c = ContractionArray(A, I, A_T=A_T, alpha=alpha)
         self._j = j
 
     def add_forward(self, b, deps):
@@ -130,37 +147,46 @@ class ContractionRHS(RHS):
     def subtract_adjoint_derivative_action(self, nl_deps, dep_index, adj_x, b):
         if dep_index < len(self._c.I()):
             A, I, alpha = self._c.A(), self._c.I(), self._c.alpha()
-            Y = [None for i in range(len(A.shape))]
+            X = [None for i in range(len(A.shape))]
             k = I[dep_index]
             for j, (i, nl_dep) in enumerate(zip(I, nl_deps)):
                 if j != dep_index:
-                    Y[i] = nl_dep
-            Y[self._j] = adj_x
+                    X[i] = nl_dep
+            X[self._j] = adj_x
 
-            b.vector()[:] -= ContractionArray(A, list(range(k)) + list(range(k + 1, len(A.shape))), A_T = self._A_T, alpha = alpha).value(Y[:k] + Y[k + 1:])
+            A_c = ContractionArray(A,
+                                   list(range(k))
+                                   + list(range(k + 1, len(A.shape))),
+                                   A_T=self._A_T, alpha=alpha)
+            b.vector()[:] -= A_c.value(X[:k] + X[k + 1:])
 
     def tangent_linear_rhs(self, M, dM, tlm_map):
-        Y = list(self.dependencies())
+        X = list(self.dependencies())
         A, I, alpha = self._c.A(), self._c.I(), self._c.alpha()
         m_map = dict(zip(M, dM))
 
         J = []
-        for j, y in enumerate(Y):
-            if y in m_map:
-                Y[j] = m_map[y]
+        for j, x in enumerate(X):
+            if x in m_map:
+                X[j] = m_map[x]
             else:
                 J.append(j)
 
         if len(J) == 0:
-            return ContractionRHS(A, I, Y, A_T = self._A_T, alpha = alpha)
+            return ContractionRHS(A, I, X, A_T=self._A_T, alpha=alpha)
         else:
             tlm_B = []
             for j in J:
-                tau_y = tlm_map[Y[j]]
-                if not tau_y is None:
-                    tlm_B.append(ContractionRHS(A, I, Y[:j] + [tau_y] + Y[j + 1:], A_T = self._A_T, alpha = alpha))
+                tau_x = tlm_map[X[j]]
+                if tau_x is not None:
+                    tlm_B.append(ContractionRHS(A, I,
+                                                X[:j] + [tau_x] + X[j + 1:],
+                                                A_T=self._A_T, alpha=alpha))
             return tlm_B
 
+
 class ContractionSolver(LinearEquation):
-    def __init__(self, A, I, Y, x, A_T = None, alpha = 1.0):
-        LinearEquation.__init__(self, ContractionRHS(A, I, Y, A_T = A_T, alpha = alpha), x)
+    def __init__(self, A, I, Y, x, A_T=None, alpha=1.0):
+        LinearEquation.__init__(self, ContractionRHS(A, I, Y, A_T=A_T,
+                                                     alpha=alpha),
+                                x)
