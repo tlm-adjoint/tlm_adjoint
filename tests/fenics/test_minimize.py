51fea10ae7e068776d9eee1d1c1d72a2c2b02658
Dr. James R. Maddison <j.r.maddison@ed.ac.uk>
2019-08-28 22:47:42 +0100

    FEniCS backend: pytest, test_minimize_scipy_multiple

--- tests/fenics/test_minimize.py
+++ tests/fenics/test_minimize.py
@@ -67,3 +67,62 @@ def test_minimize_project(setup_test, test_leaks):
     function_assign(error, alpha_ref)
     function_axpy(error, -1.0, alpha)
     assert(function_linf_norm(error) < 1.0e-7)
+
+
+@pytest.mark.fenics
+def test_minimize_project_multiple(setup_test, test_leaks):
+    mesh = UnitSquareMesh(20, 20)
+    X = SpatialCoordinate(mesh)
+    space = FunctionSpace(mesh, "Lagrange", 1)
+    test, trial = TestFunction(space), TrialFunction(space)
+
+    def forward(alpha, beta, x_ref=None, y_ref=None):
+        x = Function(space, name="x")
+        solve(inner(test, trial) * dx == inner(test, alpha) * dx,
+              x, solver_parameters=ls_parameters_cg)
+
+        y = Function(space, name="y")
+        solve(inner(test, trial) * dx == inner(test, beta) * dx,
+              y, solver_parameters=ls_parameters_cg)
+
+        if x_ref is None:
+            x_ref = Function(space, name="x_ref", static=True)
+            function_assign(x_ref, x)
+        if y_ref is None:
+            y_ref = Function(space, name="y_ref", static=True)
+            function_assign(y_ref, y)
+
+        J = Functional(name="J")
+        J.assign(inner(x - x_ref, x - x_ref) * dx)
+        J.addto(inner(y - y_ref, y - y_ref) * dx)
+        return x_ref, y_ref, J
+
+    alpha_ref = Function(space, name="alpha_ref", static=True)
+    interpolate_expression(alpha_ref, exp(X[0] + X[1]))
+    beta_ref = Function(space, name="beta_ref", static=True)
+    interpolate_expression(beta_ref, sin(pi * X[0]) * sin(2.0 * pi * X[1]))
+    x_ref, y_ref, _ = forward(alpha_ref, beta_ref)
+
+    alpha0 = Function(space, name="alpha0", static=True)
+    beta0 = Function(space, name="beta0", static=True)
+    start_manager()
+    _, _, J = forward(alpha0, beta0, x_ref=x_ref, y_ref=y_ref)
+    stop_manager()
+
+    def forward_J(alpha, beta):
+        return forward(alpha, beta, x_ref=x_ref, y_ref=y_ref)[2]
+
+    (alpha, beta), result = minimize_scipy(forward_J, (alpha0, beta0), J0=J,
+                                           method="L-BFGS-B",
+                                           options={"ftol": 0.0,
+                                                    "gtol": 1.0e-11})
+    assert(result.success)
+
+    error = Function(space, name="error")
+    function_assign(error, alpha_ref)
+    function_axpy(error, -1.0, alpha)
+    assert(function_linf_norm(error) < 1.0e-8)
+
+    function_assign(error, beta_ref)
+    function_axpy(error, -1.0, beta)
+    assert(function_linf_norm(error) < 1.0e-9)
