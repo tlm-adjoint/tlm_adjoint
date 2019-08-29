e4588b6e828f33d18a7d65f1e5ba1c944e3b6ac8
Dr. James R. Maddison <j.r.maddison@ed.ac.uk>
2019-08-29 18:21:27 +0100

    pytest, register markers

---
+++ conftest.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def pytest_configure(config):
+    config.addinivalue_line("markers", "fenics: FEniCS tests")
+    config.addinivalue_line("markers", "firedrake: Firedrake tests")
+    config.addinivalue_line("markers", "numpy: NumPy tests")
