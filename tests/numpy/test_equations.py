da0a9ad5b2db3f1dbab70d92ffc80e4d4de51a2a
Dr. James R. Maddison <j.r.maddison@ed.ac.uk>
2019-08-29 18:19:56 +0100

    NumPy backend: pytest, test_ContractionSolver

--- tests/numpy/test_equations.py
+++ tests/numpy/test_equations.py
@@ -36,7 +36,6 @@ def test_SumSolver(setup_test, test_leaks):
 
         J = Functional(name="J")
         SumSolver(G, J.fn()).solve()
-
         return J
 
     F = Function(space, name="F", static=True)
@@ -62,7 +61,6 @@ def test_InnerProductSolver(setup_test, test_leaks):
 
         J = Functional(name="J")
         InnerProductSolver(F, G, J.fn()).solve()
-
         return J
 
     F = Function(space, name="F", static=True)
@@ -75,3 +73,54 @@ def test_InnerProductSolver(setup_test, test_leaks):
     dJ = compute_gradient(J, F)
     min_order = taylor_test(forward, F, J_val=J.value(), dJ=dJ)
     assert(min_order > 1.99)
+
+
+@pytest.mark.numpy
+def test_ContractionSolver(setup_test, test_leaks):
+    space_0 = FunctionSpace(1)
+    space = FunctionSpace(3)
+    A = np.array([[1.0, 2.0, 3.0], [0.0, 4.0, 5.0], [0.0, 0.0, 6.0]],
+                 dtype=np.float64)
+
+    def forward(m):
+        x = Function(space, name="x")
+        ContractionSolver(A, (1,), (m,), x).solve()
+
+        norm_sq = Function(space_0, name="norm_sq")
+        NormSqSolver(x, norm_sq).solve()
+
+        J = Functional(name="J")
+        NormSqSolver(norm_sq, J.fn()).solve()
+        return x, J
+
+    m = Function(space, name="m", static=True)
+    function_set_values(m, np.array([7.0, 8.0, 9.0], dtype=np.float64))
+
+    start_manager()
+    x, J = forward(m)
+    stop_manager()
+
+    assert(abs(A.dot(m.vector()) - x.vector()).max() == 0.0)
+
+    J_val = J.value()
+
+    dJ = compute_gradient(J, m)
+
+    def forward_J(m):
+        return forward(m)[1]
+
+    min_order = taylor_test(forward_J, m, J_val=J_val, dJ=dJ)
+    assert(min_order > 2.00)
+
+    ddJ = Hessian(forward_J)
+    min_order = taylor_test(forward_J, m, J_val=J_val, ddJ=ddJ)
+    assert(min_order > 3.00)
+
+    min_order = taylor_test_tlm(forward_J, m, tlm_order=1)
+    assert(min_order > 2.00)
+
+    min_order = taylor_test_tlm_adjoint(forward_J, m, adjoint_order=1)
+    assert(min_order > 2.00)
+
+    min_order = taylor_test_tlm_adjoint(forward_J, m, adjoint_order=2)
+    assert(min_order > 2.00)
