d092bef1bb826d7654f890ef977e34666a7f888e
Dr. James R. Maddison <j.r.maddison@ed.ac.uk>
2020-05-25 18:22:57 +0100

    Drop some permitted Function references in the test_leaks fixtures

--- tests/fenics/test_base.py
+++ tests/fenics/test_base.py
@@ -130,7 +130,7 @@ def test_leaks():
     manager._cp_memory.clear()
     tlm_values = list(manager._tlm.values())  # noqa: F841
     manager._tlm.clear()
-    tlm_eqs_values = list(manager._tlm_eqs.values())  # noqa: F841
+    tlm_eqs_values = [list(eq_tlm_eqs.values()) for eq_tlm_eqs in manager._tlm_eqs.values()]  # noqa: E501,F841
     manager._tlm_eqs.clear()
     manager._replace_deferred()
 
