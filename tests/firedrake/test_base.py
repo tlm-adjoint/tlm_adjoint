7aa130d23b4dc708e9dbbf6e20660cfe58f80ed6
Dr. James R. Maddison <j.r.maddison@ed.ac.uk>
2020-05-25 18:07:05 +0100

    Bugfix to test_leaks fixtures

--- tests/firedrake/test_base.py
+++ tests/firedrake/test_base.py
@@ -138,9 +138,9 @@ def test_leaks():
     manager = _manager()
     manager._cp.clear(clear_refs=True)
     manager._cp_memory.clear()
-    tlm_values = manager._tlm.values()  # noqa: F841
+    tlm_values = list(manager._tlm.values())  # noqa: F841
     manager._tlm.clear()
-    tlm_eqs_values = manager._tlm_eqs.values()  # noqa: F841
+    tlm_eqs_values = list(manager._tlm_eqs.values())  # noqa: F841
     manager._tlm_eqs.clear()
     manager._replace_deferred()
 
