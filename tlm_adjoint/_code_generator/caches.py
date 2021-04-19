332b5819cb7f8de8a6ec6d2c31457e1b5facafb9
James R. Maddison <j.r.maddison@ed.ac.uk>
2021-04-19 16:18:15 +0100

    FEniCS/Firedrake backends: Remove mutable default argument values from LinearSolverCache.linear_solver

--- tlm_adjoint/_code_generator/caches.py
+++ tlm_adjoint/_code_generator/caches.py
@@ -18,7 +18,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with tlm_adjoint.  If not, see <https://www.gnu.org/licenses/>.
 
-from .backend import TrialFunction, backend_Function
+from .backend import TrialFunction, backend_DirichletBC, backend_Function
 from ..interface import function_id, function_is_cached, function_space, \
     is_function
 from .backend_code_generator_interface import assemble, assemble_arguments, \
@@ -304,9 +304,19 @@ def linear_solver_key(form, bcs, linear_solver_parameters,
 
 
 class LinearSolverCache(Cache):
-    def linear_solver(self, form, A=None, bcs=[], form_compiler_parameters={},
-                      linear_solver_parameters={}, replace_map=None,
-                      assembly_cache=None):
+    def linear_solver(self, form, A=None, bcs=None,
+                      form_compiler_parameters=None,
+                      linear_solver_parameters=None,
+                      replace_map=None, assembly_cache=None):
+        if bcs is None:
+            bcs = ()
+        elif isinstance(bcs, backend_DirichletBC):
+            bcs = (bcs,)
+        if form_compiler_parameters is None:
+            form_compiler_parameters = {}
+        if linear_solver_parameters is None:
+            linear_solver_parameters = {}
+
         form = eliminate_zeros(form, force_non_empty_form=True)
         key = linear_solver_key(form, bcs, linear_solver_parameters,
                                 form_compiler_parameters)
