64ddd759cc3699aec6f3dc67f6a8b6fbf95fb491
James R. Maddison <j.r.maddison@ed.ac.uk>
2022-02-10 13:49:27 +0000

    Move pytest marker registration to setup.cfg

--- conftest.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-
-def pytest_configure(config):
-    config.addinivalue_line("markers", "example: example scripts")
-    config.addinivalue_line("markers", "fenics: FEniCS tests")
-    config.addinivalue_line("markers", "firedrake: Firedrake tests")
-    config.addinivalue_line("markers", "numpy: NumPy tests")
