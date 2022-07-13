0d114a48da6d71522fc73824361ad39f7084013e
James R. Maddison <j.r.maddison@ed.ac.uk>
2022-07-13 11:24:55 +0100

    FEniCS backend: Optimize some cases in interpolate_expression

--- tlm_adjoint/fenics/backend_code_generator_interface.py
+++ tlm_adjoint/fenics/backend_code_generator_interface.py
@@ -402,13 +402,24 @@ def interpolate_expression(x, expr):
             return x.ufl_shape
 
     if isinstance(x, backend_Constant):
-        if len(x.ufl_shape) > 0:
-            raise ValueError("Scalar Constant required")
-        value = x.values()
-        Expr().eval(value, ())
+        if isinstance(expr, backend_Constant):
+            value = expr
+        else:
+            if len(x.ufl_shape) > 0:
+                raise ValueError("Scalar Constant required")
+            value = x.values()
+            Expr().eval(value, ())
         x.assign(value, annotate=False, tlm=False)
     elif isinstance(x, backend_Function):
-        x.interpolate(Expr())
+        if isinstance(expr, backend_Constant):
+            value = expr
+        else:
+            value = backend_Function(x.function_space())
+            try:
+                value.assign(expr, annotate=False, tlm=False)
+            except RuntimeError:
+                value.interpolate(Expr())
+        x.assign(value, annotate=False, tlm=False)
     else:
         raise TypeError(f"Unexpected type: {type(x)}")
 
