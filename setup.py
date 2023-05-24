bc9d4419b6ab112029b053a7169cca791d755027
James R. Maddison <j.r.maddison@ed.ac.uk>
2023-05-24 10:56:22 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,10 +7,11 @@ from setuptools import setup
 setup(
     name="tlm_adjoint",
     description="A library for high-level algorithmic differentiation",
-    url="https://github.com/jrmaddison/tlm_adjoint",
+    url="https://github.com/tlm-adjoint/tlm_adjoint",
     license="GNU LGPL version 3",
     packages=["tlm_adjoint",
               "tlm_adjoint._code_generator",
+              "tlm_adjoint.checkpoint_schedules",
               "tlm_adjoint.fenics",
               "tlm_adjoint.firedrake",
               "tlm_adjoint.numpy"],
