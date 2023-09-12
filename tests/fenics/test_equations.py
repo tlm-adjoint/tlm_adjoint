48abf995502962f3f6d7246e47128dddb85865f1
James R. Maddison <j.r.maddison@ed.ac.uk>
2023-09-12 16:33:21 +0100

    Add test_Assignment and test_Axpy in tests/base. Update FEniCS/Firedrake test_Assignment and test_Axpy tests.

--- tests/fenics/test_equations.py
+++ tests/fenics/test_equations.py
@@ -19,116 +19,132 @@ pytestmark = pytest.mark.skipif(
 
 
 @pytest.mark.fenics
-@no_space_type_checking
 @seed_test
 def test_Assignment(setup_test, test_leaks):
-    x = Constant(16.0, name="x", static=True)
+    mesh = UnitSquareMesh(20, 20)
+    X = SpatialCoordinate(mesh)
+    space = FunctionSpace(mesh, "Lagrange", 1)
 
     def forward(x):
-        y = [Constant(name=f"y_{i:d}") for i in range(9)]
-        z = Constant(name="z")
-
-        Assignment(y[0], x).solve()
-        for i in range(len(y) - 1):
-            Assignment(y[i + 1], y[i]).solve()
-        # Following line should have no effect on sensitivity
-        DotProduct(z, y[-1], y[-1]).solve()
-        DotProduct(z, y[-1], y[-1]).solve()
-
-        x_dot_x = Constant(name="x_dot_x")
-        DotProduct(x_dot_x, x, x).solve()
-
-        z_dot_z = Constant(name="z_dot_z")
-        DotProduct(z_dot_z, z, z).solve()
+        y = Function(x.function_space())
+        Assignment(y, x).solve()
 
         J = Functional(name="J")
-        Axpy(J.function(), z_dot_z, 2.0, x_dot_x).solve()
+        J.assign(((y - Constant(1.0)) ** 4) * dx)
+        return y, J
 
-        K = Functional(name="K")
-        Assignment(K.function(), z_dot_z).solve()
-
-        return J, K
-
-    start_manager()
-    J, K = forward(x)
-    stop_manager()
-
-    assert abs(J.value() - 66048.0) == 0.0
-    assert abs(K.value() - 65536.0) == 0.0
-
-    dJs = compute_gradient([J, K], x)
-
-    dm = Constant(1.0, name="dm", static=True)
-
-    for forward_J, J_val, dJ in [(lambda x: forward(x)[0], J.value(), dJs[0]),
-                                 (lambda x: forward(x)[1], K.value(), dJs[1])]:
-        min_order = taylor_test(forward_J, x, J_val=J_val, dJ=dJ, dM=dm)
-        assert min_order > 2.00
-
-        ddJ = Hessian(forward_J)
-        min_order = taylor_test(forward_J, x, J_val=J_val, ddJ=ddJ, dM=dm)
-        assert min_order > 3.00
-
-        min_order = taylor_test_tlm(forward_J, x, tlm_order=1, dMs=(dm,))
-        assert min_order > 2.00
-
-        min_order = taylor_test_tlm_adjoint(forward_J, x, adjoint_order=1,
-                                            dMs=(dm,))
-        assert min_order > 2.00
-
-        min_order = taylor_test_tlm_adjoint(forward_J, x, adjoint_order=2,
-                                            dMs=(dm, dm))
-        assert min_order > 2.00
-
-
-@pytest.mark.fenics
-@no_space_type_checking
-@seed_test
-def test_Axpy(setup_test, test_leaks):
-    x = Constant(1.0, name="x", static=True)
-
-    def forward(x):
-        y = [Constant(name=f"y_{i:d}") for i in range(5)]
-        z = [Constant(name=f"z_{i:d}") for i in range(2)]
-        z[0].assign(7.0)
-
-        Assignment(y[0], x).solve()
-        for i in range(len(y) - 1):
-            Axpy(y[i + 1], y[i], i + 1, z[0]).solve()
-        DotProduct(z[1], y[-1], y[-1]).solve()
-
-        J = Functional(name="J")
-        DotProduct(J.function(), z[1], z[1]).solve()
-        return J
+    x = Function(space, name="x")
+    if complex_mode:
+        interpolate_expression(
+            x,
+            sin(pi * X[0]) * sin(3.0 * pi * X[1])
+            + 1.0j * sin(5.0 * pi * X[0]) * sin(7.0 * pi * X[1]))
+    else:
+        interpolate_expression(
+            x,
+            sin(pi * X[0]) * sin(3.0 * pi * X[1]))
 
     start_manager()
-    J = forward(x)
+    y, J = forward(x)
     stop_manager()
+    assert function_state(x) == 1
+
+    y_error = function_copy(y)
+    function_axpy(y_error, -1.0, x)
+    assert function_linf_norm(y_error) == 0.0
 
     J_val = J.value()
-    assert abs(J_val - 25411681.0) == 0.0
 
     dJ = compute_gradient(J, x)
 
-    dm = Constant(1.0, name="dm", static=True)
+    def forward_J(x):
+        _, J = forward(x)
+        return J
 
-    min_order = taylor_test(forward, x, J_val=J_val, dJ=dJ, dM=dm)
-    assert min_order > 2.00
+    min_order = taylor_test(forward_J, x, J_val=J_val, dJ=dJ)
+    assert min_order > 1.99
 
-    ddJ = Hessian(forward)
-    min_order = taylor_test(forward, x, J_val=J_val, ddJ=ddJ, dM=dm,
-                            seed=2.0e-2)
-    assert min_order > 3.00
+    ddJ = Hessian(forward_J)
+    min_order = taylor_test(forward_J, x, J_val=J_val, ddJ=ddJ)
+    assert min_order > 2.99
 
-    min_order = taylor_test_tlm(forward, x, tlm_order=1, dMs=(dm,))
-    assert min_order > 2.00
+    min_order = taylor_test_tlm(forward_J, x, tlm_order=1)
+    assert min_order > 1.99
 
-    min_order = taylor_test_tlm_adjoint(forward, x, adjoint_order=1, dMs=(dm,))
-    assert min_order > 2.00
+    min_order = taylor_test_tlm_adjoint(forward_J, x, adjoint_order=1)
+    assert min_order > 1.99
 
-    min_order = taylor_test_tlm_adjoint(forward, x, adjoint_order=2,
-                                        dMs=(dm, dm))
-    assert min_order > 2.00
+    min_order = taylor_test_tlm_adjoint(forward_J, x, adjoint_order=2)
+    assert min_order > 1.99
+
+
+@pytest.mark.fenics
+@pytest.mark.parametrize("c", [3.0, -3.0])
+@seed_test
+def test_Axpy(setup_test, test_leaks,
+              c):
+    mesh = UnitSquareMesh(20, 20)
+    X = SpatialCoordinate(mesh)
+    space = FunctionSpace(mesh, "Lagrange", 1)
+
+    def forward(x):
+        y_0 = Function(x.function_space())
+        Assignment(y_0, x).solve()
+
+        y_1 = Function(x.function_space())
+        y_1.assign(Constant(2.0))
+
+        y_2 = Function(x.function_space())
+        Axpy(y_2, y_0, c, y_1).solve()
+
+        J = Functional(name="J")
+        J.assign(((y_2 - Constant(1.0)) ** 4) * dx)
+        return y_2, J
+
+    x = Function(space, name="x")
+    if complex_mode:
+        interpolate_expression(
+            x,
+            sin(pi * X[0]) * sin(3.0 * pi * X[1])
+            + 1.0j * sin(5.0 * pi * X[0]) * sin(7.0 * pi * X[1]))
+    else:
+        interpolate_expression(
+            x,
+            sin(pi * X[0]) * sin(3.0 * pi * X[1]))
+
+    start_manager()
+    y, J = forward(x)
+    stop_manager()
+    assert function_state(x) == 1
+
+    y_error = function_copy(y)
+    function_axpy(y_error, -1.0, x)
+    function_axpy(y_error, -c, 2.0)
+    assert function_linf_norm(y_error) == 0.0
+
+    J_val = J.value()
+
+    dJ = compute_gradient(J, x)
+
+    def forward_J(x):
+        _, J = forward(x)
+        return J
+
+    min_order = taylor_test(forward_J, x, J_val=J_val, dJ=dJ)
+    assert min_order > 1.99
+
+    ddJ = Hessian(forward_J)
+    min_order = taylor_test(forward_J, x, J_val=J_val, ddJ=ddJ)
+    assert min_order > 2.99
+
+    min_order = taylor_test_tlm(forward_J, x, tlm_order=1)
+    assert min_order > 1.99
+
+    min_order = taylor_test_tlm_adjoint(forward_J, x, adjoint_order=1)
+    assert min_order > 1.99
+
+    min_order = taylor_test_tlm_adjoint(forward_J, x, adjoint_order=2)
+    assert min_order > 1.99
 
 
 @pytest.mark.fenics
