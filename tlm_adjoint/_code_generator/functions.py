57d6e88057ced773fbfd14fef754a06e1c2f9683
James R. Maddison <j.r.maddison@ed.ac.uk>
2023-09-13 08:38:21 +0100

    Bugfix to static DirichletBC objects

--- tlm_adjoint/_code_generator/functions.py
+++ tlm_adjoint/_code_generator/functions.py
@@ -640,11 +640,11 @@ class DirichletBC(backend_DirichletBC):
         if static is None:
             for dep in extract_coefficients(
                     g if isinstance(g, ufl.classes.Expr)
-                    else backend_Constant(g)):
+                    else Constant(g, static=True)):
                 # The 'static' flag for functions is only a hint. 'not
                 # checkpointed' is a guarantee that the function will never
                 # appear as the solution to an Equation.
-                if not is_function(dep) or not function_is_checkpointed(dep):
+                if not is_function(dep) or function_is_checkpointed(dep):
                     static = False
                     break
             else:
