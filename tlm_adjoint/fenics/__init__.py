76b7c40a0a564fc2331bd52fed97f566c5af79ed
James R. Maddison <j.r.maddison@ed.ac.uk>
2023-10-17 16:52:21 +0100

    FEniCS backend: Remove the block system code

--- tlm_adjoint/fenics/__init__.py
+++ tlm_adjoint/fenics/__init__.py
@@ -12,10 +12,7 @@ from .backend import backend, backend_RealType, backend_ScalarType  # noqa: E402
 from .backend_code_generator_interface import linear_solver  # noqa: E402,F401
 from .backend_interface import *   # noqa: E402,F401
 from .backend_overrides import *   # noqa: E402,F401
-from .block_system import \
-    (ConstantNullspace, DirichletBCNullspace, NoneNullspace, UnityNullspace)  # noqa: E402,E501,F401
 from .caches import *              # noqa: E402,F401
 from .equations import *           # noqa: E402,F401
 from .fenics_equations import *    # noqa: E402,F401
 from .functions import *           # noqa: E402,F401
-from .hessian_system import *      # noqa: E402,F401
