2f61d682afa4a816737ecda33b50692bcccf43d2
James R. Maddison <j.r.maddison@ed.ac.uk>
2023-12-04 09:47:39 +0000

    Update project override tests

--- tests/firedrake/test_overrides.py
+++ tests/firedrake/test_overrides.py
@@ -18,102 +18,119 @@ pytestmark = pytest.mark.skipif(
     reason="tests must be run in serial, or with 4 processes")
 
 
+def project_project(F, space, bc):
+    return project(F, space, bcs=bc, name="G",
+                   solver_parameters=ls_parameters_cg)
+
+
+def project_assemble_LinearSolver(F, space, bc):
+    test, trial = TestFunction(space), TrialFunction(space)
+    G = Function(space, name="G")
+
+    A = assemble(inner(trial, test) * dx, bcs=bc)
+    b = assemble(inner(F, test) * dx)
+
+    solver = LinearSolver(A, solver_parameters=ls_parameters_cg)
+    solver.solve(G, b)
+
+    return G
+
+
+def project_LinearVariationalSolver(F, space, bc):
+    test, trial = TestFunction(space), TrialFunction(space)
+    G = Function(space, name="G")
+
+    eq = inner(trial, test) * dx == inner(F, test) * dx
+    problem = LinearVariationalProblem(eq.lhs, eq.rhs, G, bcs=bc)
+    solver = LinearVariationalSolver(
+        problem, solver_parameters=ls_parameters_cg)
+    solver.solve()
+
+    return G
+
+
+def project_NonlinearVariationalSolver(F, space, bc):
+    test, trial = TestFunction(space), TrialFunction(space)
+    G = Function(space, name="G")
+
+    eq = inner(G, test) * dx - inner(F, test) * dx
+    problem = NonlinearVariationalProblem(eq, G,
+                                          J=inner(trial, test) * dx,
+                                          bcs=bc)
+    solver = NonlinearVariationalSolver(
+        problem, solver_parameters=ns_parameters_newton_cg)
+    solver.solve()
+
+    return G
+
+
 @pytest.mark.firedrake
+@pytest.mark.parametrize("project_fn", [project_project,
+                                        project_assemble_LinearSolver,
+                                        project_LinearVariationalSolver,
+                                        project_NonlinearVariationalSolver])
 @seed_test
-def test_overrides(setup_test, test_leaks):
+def test_project_overrides(setup_test, test_leaks,
+                           project_fn):
     mesh = UnitSquareMesh(20, 20)
     X = SpatialCoordinate(mesh)
     space = FunctionSpace(mesh, "Lagrange", 1)
     test, trial = TestFunction(space), TrialFunction(space)
 
-    F = Function(space, name="F", static=True)
-    interpolate_expression(F, sin(pi * X[0]) * sin(3.0 * pi * X[1]))
+    F = Function(FunctionSpace(mesh, "Lagrange", 2), name="F", static=True)
+    if complex_mode:
+        interpolate_expression(
+            F,
+            sin(pi * X[0]) * sin(3.0 * pi * X[1])
+            + 1.0j * sin(5.0 * pi * X[0]) * sin(7.0 * pi * X[1]))
+    else:
+        interpolate_expression(
+            F,
+            sin(pi * X[0]) * sin(3.0 * pi * X[1]))
 
     bc = DirichletBC(space, 1.0, "on_boundary")
 
-    def project_project(F):
-        return project(F, space, bcs=bc, name="G",
-                       solver_parameters=ls_parameters_cg)
+    def forward(F):
+        G = project_fn(F, space, bc)
 
-    def project_assemble_LinearSolver(F):
-        G = Function(space, name="G")
+        J = Functional(name="J")
+        J.assign(G * G * (Constant(1.0) + G) * dx)
+        return G, J
 
-        A = assemble(inner(trial, test) * dx, bcs=bc)
-        b = assemble(inner(F, test) * dx)
+    F_ref = F.copy(deepcopy=True)
+    start_manager()
+    G, J = forward(F)
+    stop_manager()
 
-        solver = LinearSolver(A, solver_parameters=ls_parameters_cg)
-        solver.solve(G, b)
+    error = Function(space, name="error")
+    solve(inner(trial, test) * dx == inner(F_ref, test) * dx,
+          error, bc, solver_parameters=ls_parameters_cg)
+    var_axpy(error, -1.0, G)
+    assert var_linf_norm(error) < 1.0e-13
 
-        return G
+    J_val = J.value
 
-    def project_LinearVariationalSolver(F):
-        G = Function(space, name="G")
+    dJ = compute_gradient(J, F)
 
-        eq = inner(trial, test) * dx == inner(F, test) * dx
-        problem = LinearVariationalProblem(eq.lhs, eq.rhs, G, bcs=bc)
-        solver = LinearVariationalSolver(
-            problem, solver_parameters=ls_parameters_cg)
-        solver.solve()
+    def forward_J(F):
+        _, J = forward(F)
+        return J
 
-        return G
+    min_order = taylor_test(forward_J, F, J_val=J_val, dJ=dJ)
+    assert min_order > 2.00
 
-    def project_NonlinearVariationalSolver(F):
-        G = Function(space, name="G")
+    ddJ = Hessian(forward_J)
+    min_order = taylor_test(forward_J, F, J_val=J_val, ddJ=ddJ)
+    assert min_order > 2.99
 
-        eq = inner(G, test) * dx - inner(F, test) * dx
-        problem = NonlinearVariationalProblem(eq, G,
-                                              J=inner(trial, test) * dx,
-                                              bcs=bc)
-        solver = NonlinearVariationalSolver(
-            problem, solver_parameters=ns_parameters_newton_cg)
-        solver.solve()
+    min_order = taylor_test_tlm(forward_J, F, tlm_order=1)
+    assert min_order > 2.00
 
-        return G
+    min_order = taylor_test_tlm_adjoint(forward_J, F, adjoint_order=1)
+    assert min_order > 2.00
 
-    for project_fn in (project_project,
-                       project_assemble_LinearSolver,
-                       project_LinearVariationalSolver,
-                       project_NonlinearVariationalSolver):
-        def forward(F):
-            G = project_fn(F)
-
-            J = Functional(name="J")
-            J.assign(dot(G, G * (1 + G)) * dx)
-            return G, J
-
-        reset_manager("memory", {"drop_references": True})
-        start_manager()
-        G, J = forward(F)
-        stop_manager()
-
-        error = Function(space, name="error")
-        solve(inner(trial, test) * dx == inner(F, test) * dx,
-              error, bc, solver_parameters=ls_parameters_cg)
-        var_axpy(error, -1.0, G)
-        assert var_linf_norm(error) < 1.0e-14
-
-        J_val = J.value
-
-        dJ = compute_gradient(J, F)
-
-        def forward_J(F):
-            return forward(F)[1]
-
-        min_order = taylor_test(forward_J, F, J_val=J_val, dJ=dJ)
-        assert min_order > 2.00
-
-        ddJ = Hessian(forward_J)
-        min_order = taylor_test(forward_J, F, J_val=J_val, ddJ=ddJ)
-        assert min_order > 2.99
-
-        min_order = taylor_test_tlm(forward_J, F, tlm_order=1)
-        assert min_order > 2.00
-
-        min_order = taylor_test_tlm_adjoint(forward_J, F, adjoint_order=1)
-        assert min_order > 2.00
-
-        min_order = taylor_test_tlm_adjoint(forward_J, F, adjoint_order=2)
-        assert min_order > 1.99
+    min_order = taylor_test_tlm_adjoint(forward_J, F, adjoint_order=2)
+    assert min_order > 1.99
 
 
 @pytest.mark.firedrake
