3c68d49dfdcd3ebf80777bfaec31f963b07ef0a8
James R. Maddison <j.r.maddison@ed.ac.uk>
2024-02-15 13:16:14 +0000

    petsc4py and slepc4py initialization

--- tlm_adjoint/__init__.py
+++ tlm_adjoint/__init__.py
@@ -1,23 +1,45 @@
-from .cached_hessian import *      # noqa: F401
-from .caches import *              # noqa: F401
-from .eigendecomposition import *  # noqa: F401
-from .equation import *            # noqa: F401
-from .equations import *           # noqa: F401
-from .fixed_point import *         # noqa: F401
-from .functional import *          # noqa: F401
-from .hessian import *             # noqa: F401
-from .instructions import *        # noqa: F401
-from .interface import *           # noqa: F401
-from .jax import *                 # noqa: F401
-from .linear_equation import *     # noqa: F401
-from .manager import *             # noqa: F401
-from .optimization import *        # noqa: F401
-from .overloaded_float import *    # noqa: F401
-from .patch import *               # noqa: F401
-from .storage import *             # noqa: F401
-from .tangent_linear import *      # noqa: F401
-from .tlm_adjoint import *         # noqa: F401
-from .verification import *        # noqa: F401
+# flake8: noqa
 
-from .hessian import GeneralGaussNewton as GaussNewton  # noqa: F401
-from .hessian import GeneralHessian as Hessian          # noqa: F401
+def _init():
+    try:
+        import petsc4py
+    except ImportError:
+        petsc4py = None
+    try:
+        import slepc4py
+    except ImportError:
+        slepc4py = None
+    import sys
+
+    if slepc4py is not None:
+        slepc4py.init(sys.argv)
+    if petsc4py is not None:
+        petsc4py.init(sys.argv)
+
+
+_init()
+del _init
+
+from .cached_hessian import *
+from .caches import *
+from .eigendecomposition import *
+from .equation import *
+from .equations import *
+from .fixed_point import *
+from .functional import *
+from .hessian import *
+from .instructions import *
+from .interface import *
+from .jax import *
+from .linear_equation import *
+from .manager import *
+from .optimization import *
+from .overloaded_float import *
+from .patch import *
+from .storage import *
+from .tangent_linear import *
+from .tlm_adjoint import *
+from .verification import *
+
+from .hessian import GeneralGaussNewton as GaussNewton
+from .hessian import GeneralHessian as Hessian
