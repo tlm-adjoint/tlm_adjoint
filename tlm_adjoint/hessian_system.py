426c627cc746418fc11ef1865e6164624bfac194
James R. Maddison <j.r.maddison@ed.ac.uk>
2024-06-04 10:47:06 +0100

    numpydoc: hessian_system

--- tlm_adjoint/hessian_system.py
+++ tlm_adjoint/hessian_system.py
@@ -1,3 +1,6 @@
+"""Hessian matrix utilities.
+"""
+
 from .interface import (
     Packed, packed, var_copy, var_copy_conjugate, var_increment_state_lock,
     var_is_cached, var_is_static, var_locked, var_space, vars_assign,
@@ -52,18 +55,26 @@ class HessianMatrix(Matrix):
 
 
 class HessianLinearSolver(LinearSolver):
-    """Defines a linear system involving a Hessian matrix,
+    """Solver for linear systems involving a Hessian matrix.
+
+    Solves the linear system
 
     .. math::
 
-        H u = b.
+        H u = b
 
-    :arg H: A :class:`.Hessian` defining :math:`H`.
-    :arg M: A variable or a :class:`Sequence` of variables defining the
-        control and its value.
+    for :math:`u` using PETSc.
 
-    Remaining arguments are passed to the
-    :class:`tlm_adjoint.block_system.LinearSolver` constructor.
+    Parameters
+    ----------
+
+    H : :class:`.Hessian`
+        Defines :math:`H`.
+    M : variable or Sequence[variable]
+        Defines the control and its value.
+    args, kwargs
+        Passed to the :class:`tlm_adjoint.block_system.LinearSolver`
+        constructor.
     """
 
     def __init__(self, H, M, *args, **kwargs):
@@ -71,17 +82,26 @@ class HessianLinearSolver(LinearSolver):
 
     @manager_disabled()
     def solve(self, u, b, *args, **kwargs):
-        """Solve a linear system involving a Hessian matrix,
+        """Solve a linear system involving a Hessian matrix.
+
+        Solves the linear system
 
         .. math::
 
-            H u = b.
+            H u = b
 
-        :arg u: Defines the solution :math:`u`.
-        :arg b: Defines the conjugate of the right-hand-side :math:`b`.
+        for :math:`u`.
 
-        Remaining arguments are handed to the
-        :meth:`tlm_adjoint.block_system.LinearSolver.solve` method.
+        Parameters
+        ----------
+
+        u : variable or Sequence[variable]
+            The solution :math:`u`. Also defines the initial guess.
+        b : variable or Sequence[variable]
+            The conjugate of the right-hand-side :math:`b`.
+        args, kwargs
+            Passed to the :meth:`tlm_adjoint.block_system.LinearSolver.solve`
+            method.
         """
 
         b_conj = Packed(b).mapped(var_copy_conjugate)
@@ -89,27 +109,35 @@ class HessianLinearSolver(LinearSolver):
 
 
 class HessianEigensolver(Eigensolver):
-    r"""Defines an eigenproblem involving a Hessian matrix
+    r"""Solver for generalized eigenproblems involving a Hessian matrix.
+
+    Solves the eigenproblem
 
     .. math::
 
-        H v = \lambda B v.
+        H v = \lambda B v
 
-    :arg H: A :class:`.Hessian` defining :math:`H`.
-    :arg M: A variable or a :class:`Sequence` of variables defining the
-        control and its value.
-    :arg B_action: A callable defining the conjugate of the action of :math:`B`
-        on some direction. Accepts one or more variables as arguments, defining
-        the direction, and returns a variable or a :class:`Sequence` of
-        variables defining the conjugate of the action on this direction.
-        Arguments should not be modified.
-    :arg B_inv_action: A callable defining the the action of :math:`B^{-1}` on
-        the conjugate of some direction. Accepts one or more variables as
-        arguments, defining the direction, and returns a variable or a
-        :class:`Sequence` of variables defining the action on this direction.
-        Arguments should not be modified.
+    using SLEPc.
 
-    Remaining arguments are passed to the :class:`.Eigensolver` constructor.
+    Parameters
+    ----------
+
+    H : :class:`.Hessian`
+        Defines :math:`H`.
+    M : variable or Sequence[variable]
+        Defines the control and its value.
+    B_action : callable
+        Accepts one or more variables as arguments, defining the direction, and
+        returns a variable or a :class:`Sequence` of variables defining the
+        conjugate of the action of :math:`B` on this direction. Arguments
+        should not be modified.
+    B_inv_action : callable
+        Accepts one or more variables as arguments, defining the direction, and
+        returns a variable or a :class:`Sequence` of variables defining the
+        action of :math:`B^{-1}` on the conjugate of this direction. Arguments
+        should not be modified.
+    args, kwargs
+        Passed to the :class:`.Eigensolver` constructor.
     """
 
     def __init__(self, H, M, B_action, B_inv_action, *args, **kwargs):
@@ -137,16 +165,35 @@ class HessianEigensolver(Eigensolver):
         super().__init__(A, B, B_inv=B_inv, *args, **kwargs)
 
     def spectral_approximation_solve(self, b):
-        r"""Compute the approximate action of an inverse Hessian action -- see
-        :meth:`.HessianEigensolver.spectral_pc_fn`. Note that this computes the
-        approximation of the action of :math:`(A + B)^{-1}` where :math:`A` and
-        :math:`B` are the matrices defining the eigenproblem solved by this
-        :class:`.HessianEigensolver`.
+        """:math:`(H + B)^{-1}` action approximation.
 
-        :arg b: A variable or :class:`Sequence` of variables defining the
-            direction for which the action is evaluated.
-        :returns: A variable or :class:`Sequence` of variables storing the
-            action.
+        Computes an approximation for
+
+        .. math ::
+
+            (H + B)^{-1} b
+
+        where :math:`H` and :math:`B` define the eigenproblem solved by this
+        :class:`.HessianEigensolver`. The approximation is constructed using
+        a partial eigenspectrum -- see
+        :meth:`.HessianEigensolver.spectral_pc_fn`.
+
+        Parameters
+        ----------
+
+        b : variable or Sequence[variable]
+            Defines :math:`b`.
+
+        Returns
+        -------
+
+        variable or Sequence[variable]
+            The approximation for the action on :math:`b`.
+
+        See also
+        --------
+
+        :meth:`.HessianEigensolver.spectral_pc_fn`
         """
 
         if not self.is_hermitian_and_positive():
@@ -166,29 +213,28 @@ class HessianEigensolver(Eigensolver):
         return b_packed.unpack(u)
 
     def spectral_pc_fn(self):
-        r"""Construct a Hessian matrix preconditioner using a partial spectrum
-        generalized eigendecomposition. Assumes that the Hessian matrix
-        consists of two terms
+        r"""Construct a partial eigenspectrum preconditioner.
+
+        Constructs a matrix preconditioner using a partial eigenspectrum.
+        Specifically for a matrix
 
         .. math::
 
-            H = A + B,
+            C = H + B,
 
-        where :math:`A` and :math:`B` are the matrices defining the
-        eigenproblem solved by this :class:`.HessianEigensolver`.
-
-        The approximation is defined via
+        where :math:`H` and :math:`B` define the eigenproblem solved by this
+        :class:`.HessianEigensolver`, the approximation is defined via
 
         .. math::
 
-            H^{-1} \approx B
+            C^{-1} \approx B^{-1}
                 - V \Lambda \left( I + \Lambda \right)^{-1} V^*
 
         where
 
         .. math::
 
-            A V = B V \Lambda,
+            H V = B V \Lambda,
 
         and where :math:`\Lambda` is a diagonal matrix and :math:`V` has
         :math:`B`-orthonormal columns, :math:`V^* B V = I`.
@@ -205,7 +251,11 @@ class HessianEigensolver(Eigensolver):
 
         See in particular their equation (20).
 
-        :returns: A callable suitable for use as the `pc_fn` argument to
+        Returns
+        -------
+
+        callable
+            Suitable for use as the `pc_fn` argument to
             :meth:`.HessianLinearSolver.solve`.
         """
 
